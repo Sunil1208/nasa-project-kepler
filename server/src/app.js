5c1d4169b3cfbf04cea9c6345766b242cdf5e038
Sunil Kumar <skmy03@gmail.com>
2023-02-26 12:33:42 +0530

    fixed cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
+const { corsOptions } = require("./config");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
-const app = express();
-app.use(express.json());
 
+const app = express();
+
+// app.use(cors({
+//     origin: "http://localhost:3000"
+// }));
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/planets", planetsRouter);
 
 module.exports = app;
