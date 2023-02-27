fdac8520028b29984750add225b82e46e9d9134b
Sunil Kumar <skmy03@gmail.com>
2023-02-27 12:50:19 +0530

    serving frontend build in server as static file, config of it and updated .gitignore file

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const { corsOptions } = require("./config");
 
@@ -13,7 +14,11 @@ const app = express();
 // }));
 
 app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "..", "public"))); // serving the frontend build as staic file (frontend production)
 app.use(express.json());
 app.use("/planets", planetsRouter);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+});
 
 module.exports = app;
--- server/src/config.js
+++ server/src/config.js
@@ -1,13 +1,14 @@
-const whitelist = ["http://localhost:3000"]
+const whitelist = ["http://localhost:3000", "http://localhost:8000"]
 
 const corsOptions = {
-    origin: function(origin, callback) {
-        if(whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    }
+    // origin: function(origin, callback) {
+    //     if(whitelist.indexOf(origin) !== -1) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error("Not allowed by CORS"))
+    //     }
+    // }
+    origin: "*"
 };
 
 module.exports = {
