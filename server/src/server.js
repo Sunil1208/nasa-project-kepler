72d0eab78cc63e984a770624a5954620705107db
Sunil Kumar <skmy03@gmail.com>
2023-03-09 17:24:41 +0530

    configured mongo connection using mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = require("./app");
 
@@ -6,9 +8,20 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER_ID}:${process.env.MONGO_PASSWORD}@cluster0.qhpvxgl.mongodb.net/?retryWrites=true&w=majority`
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+    console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.error("err is ", err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
